8872eb24f6646060d0f1eb35d0c5ec9f64a6b578
adsovetzky <adsovetzky@gmail.com>
2025-10-21 07:34:36 +0700

    hey chat

--- DE_THI_2025/DAKLAK/bai3.cpp
+++ DE_THI_2025/DAKLAK/bai3.cpp
@@ -1,7 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+long long SUM(int n, vector<long long> u, vector<long long> v) {
+  int ans = 0;
+  auto posu = upper_bound(u.begin(), u.end(), n);
+  for (int i = 0; i <= posu - u.begin() - 1; i++) {
+    ans++;
+  }
+  auto posv = lower_bound(v.begin(), v.end(), n);
+  for (int i = 0; i <= posv - v.begin() - 1; i++) {
+    ans--;
+  }
+  return ans;
+}
+
 int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
+  long long n, ans;
+  cin >> n;
+  vector<long long> upos, vpos;
+  for (long long i = 0; i < n; i++) {
+    long long u, v;
+    cin >> u >> v;
+    upos.push_back(u);
+    vpos.push_back(v);
+  }
+  sort(upos.begin(), upos.end());
+  sort(vpos.begin(), vpos.end());
+  long long q;
+  cin >> q;
+  for (long long i = 0; i < q; i++) {
+    long long x;
+    cin >> x;
+    ans = SUM(x, upos, vpos);
+  }
+  cout << ans;
 }
